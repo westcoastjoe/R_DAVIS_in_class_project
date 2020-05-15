f6140d589321399d5c260a444f63932fefe09fb1
Jonas <jcalsbeek@ucdavis.edu>
2020-05-14 19:11:10 -0700

    week 2 notes from class

---
+++ week_2.R
@@ -0,0 +1,29 @@
+#welcome to week 2!
+
+x <- 5
+
+y <- x * 2
+
+x <- 20
+
+y <- x*2
+
+#it can be tricky if i use = instead of <- , because = should be used for definitions in equations
+y = x * 2
+
+#how r talks back
+log_of_word <- log("word")
+
+log_of_word
+
+#errors stop the whole process and don't give outputs other than the error
+
+log_of_negative <- log(-2)
+
+log_of_negative
+
+#warnings can predict problems in the future, not going to stop everything like an error would
+
+#messages aren't anything to worry about
+
+message("you guys are doing great")
