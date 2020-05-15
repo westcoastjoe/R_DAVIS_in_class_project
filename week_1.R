93dc8f6f405b4f24694322bf05371755a401d3b5
Jonas <jcalsbeek@ucdavis.edu>
2020-05-14 17:55:18 -0700

    week 1 classwork

---
+++ week_1.R
@@ -0,0 +1,77 @@
+1+100
+1+100
+
+#r uses order of operations
+
+3 + 5 *2
+
+#use parentheses to force order of operations
+
+(3 + 5) *2
+3+5
+3 + 5
+
+#use comments often and liberally, my worst enemy is myself 3 months ago
+
+#scientific notation
+
+2 / 10000
+
+1e9 * 8
+
+#functions in r
+
+sin(3.14)
+
+log(3)
+
+exp(1/2)
+
+#nested functions, interpreted from the inside out
+
+sqrt(exp(4))
+
+#comparisons in R
+
+#R can do logical comparison
+
+1 == 1
+
+1 == 3
+
+1 != 2
+
+#objects and assignments in R
+
+x <- 1/4
+
+log(x)
+
+x <- 99
+ 
+x <- x+1
+
+y <- x * 10
+
+this_is_my_object <- 90
+
+#can't have spaces or start with a number
+#make it descriptive so i remember what it is
+
+log(this_is_my_object) #tabcompletion
+
+log(3, 10)
+
+log(x=3, base=10) #same as above
+
+log(10, 3)
+
+log(base=10, x=3)
+#still works but I need to define
+
+log(x = 10, base = 3)
+#this was completed using tab completion to autofill arguments or objects
+
+log(aword)
+#r needs something numerical
+
