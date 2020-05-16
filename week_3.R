46444b5495e25e02330ed37f9d22b36d0af19df3
Jonas <jcalsbeek@ucdavis.edu>
2020-05-15 22:31:45 -0700

    final notes week 3

--- week_3.R
+++ week_3.R
@@ -93,4 +93,88 @@ z <- 1:2
 
 x + z
 
-test
+a <- x + z
+a
+
+x
+
+z
+
+length(x)
+length(y)
+
+#recycling with conditional subsetting
+x[c(TRUE, FALSE)]
+
+x[c(TRUE, FALSE, FALSE)]
+
+#working with missing data
+
+#in R missing data are represented as NAs
+
+heights <- c(2, 4, 4, NA, 6)
+
+mean (heights)
+
+#na.rm = NA remome
+mean(heights, na.rm = TRUE)
+
+max(heights, na.rm = TRUE)
+
+class(heights) #numeric, NA is not a character, R views it as a number, sort of
+
+heights
+
+#is.NA = is it a NA?
+
+is.na(heights)
+
+#! = is not
+!is.na(heights)
+
+heights[!is.na(heights)]
+
+#complete.cases extracting all the elements that are complete cases (are not NAs)
+
+heights[complete.cases(heights)]
+
+#challenge 4, remove the NAs
+
+heights <- c(63, 69, 60, 65, NA, 68, 61, 70, 61, 59, 64, 69, 63, 63, NA, 72, 65, 64, 70, 63, 65)
+
+median(heights)
+
+median(heights, na.rm = TRUE)
+
+heights[heights > 67]
+
+heights[complete.cases(heights > 67)]
+
+heights2 <- heights[complete.cases(heights)]
+heights2
+
+median(heights2)
+
+#longer than 67 inches
+length(heights2[heights2 > 67])
+
+#factors are a bit funky
+
+sex <- factor(c("male", "female", "female", "male"))
+class(sex)
+typeof(sex)
+
+levels(sex)
+
+nlevels(sex)
+
+#specify the order of the levels
+
+sex <- factor(sex, levels = c("male", "female"))
+
+sex
+
+#making factors into characters
+
+sex_ch <- as.character(sex)
+
