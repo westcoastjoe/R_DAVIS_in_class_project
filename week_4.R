cbf06ae5ca24f96b5ac33c5914368b4184bcbc2e
jonas <jcalsbeek@ucdavis.edu>
2020-05-17 14:55:07 -0700

    completed week 4 classwork

--- week_4.R
+++ week_4.R
@@ -104,5 +104,65 @@ surveys[,-1]
 surveys["species_id"]
 surveys[1:6, "species_id"]
 
-#challenge:  
+#challenge:  create a new data frame of the first 200 rows
+
+surveys_200 <- surveys[200,]
+
+nrow(surveys)
+
+surveys[34786,]
+
+tail(surveys)
+
+nrows <- nrow(surveys)
+
+surveys[nrows,]
+#nrows fills in for the long number so we don't have to type it out each time
+
+#getting the exact middle row of the data
+
+surveys[nrows/2,]
+
+surveys_head <- surveys[-c(7:nrows)]
+surveys_head2 <- surveys[-c(7:nrows),]
+
+#the comma is necessary to clarify we want rows, otherwise it assumes columns
+
+#TIDYVERSE HUZZAH
+
+#install.packages("tidyverse") how to install a package
+
+#how to load a package
+library(tidyverse)
+
+surveys <- read_csv("data/raw_data/portal_data_joined.csv")
+
+surveys
+
+class(surveys)
+
+#dplyer is an awesome package for manipulating data
+
+#select and filter
+
+#select is for columns, and filter is for rows
+
+surveys_new <- select(surveys, plot_id, species_id, weight)
+surveys_new
+
+filter(surveys, plot_id == 3)
+
+#let's talk about pipes
+
+# I want all weights greater than 5 and only columns species_id, sex, and weight
+
+surveys2 <- filter(surveys, weight > 5)
+surveys_sml <- select(surveys2, species_id, sex, weight)
+
+# a pipe looks like this %>% (shortcut = ctrl+shift+m)
+
+survey_sml2 <- surveys %>% 
+  filter(weight > 5) %>% 
+  select(species_id, sex, weight)
+
 
